efddbd5ce7ea2afa4871405a28e54c2089456703
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-09-15 23:29:29 +0200

    Changed the Post.cs

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,42 +13,19 @@ namespace CampusLearn.Models
         public int TopicID { get; set; }
 
         [ForeignKey("Student")]
-        public int? StudentID { get; set; }
+        public int? StudentID { get; set; } // Nullable for anonymous posts
 
-        [ForeignKey("ParentPost")]
-        public int? ParentPostID { get; set; }
+        [ForeignKey("Post")]
+        public int? ParentPostID { get; set; } // For threaded replies
 
         [Required]
-        public required string Content { get; set; }
+        public string Content { get; set; }
 
         [Required]
         public DateTime DatePosted { get; set; } = DateTime.Now;
 
-        public bool IsFlagged { get; set; } = false;
-
-        public virtual required Topic Topic { get; set; }
-        public virtual Student? Student { get; set; }
-        public virtual required Post ParentPost { get; set; }
-        public virtual required ICollection<Post> Replies { get; set; }
-        public virtual required ICollection<Upvote> Upvotes { get; set; }
-
-        public Post Reply(string content, Student? student = null)
-        {
-            return new Post
-            {
-                Topic = Topic,
-                ParentPost = this,
-                Student = student,
-                Content = content,
-                DatePosted = DateTime.Now,
-                Replies = new List<Post>(),
-                Upvotes = new List<Upvote>()
-            };
-        }
-
-        public void Edit(string content)
-        {
-            Content = content;
-        }
+        
+        public Post Reply(string content) { return null; }
+        public void Edit(string content) { }
     }
 }
