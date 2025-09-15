2758e6edea008eaf096ce2e72340949e2e0e8efb
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-09-15 23:38:02 +0200

    Minute changes to Sub.cs

--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -6,18 +6,13 @@ namespace CampusLearn.Models
 {
     public class Subscription
     {
-        [Key, Column(Order = 0)]
         [ForeignKey("Student")]
         public int StudentID { get; set; }
 
-        [Key, Column(Order = 1)]
         [ForeignKey("Topic")]
         public int TopicID { get; set; }
 
         public DateTime SubscribedDate { get; set; } = DateTime.Now;
-
-        public virtual Student Student { get; set; }
-        public virtual Topic Topic { get; set; }
 
         public void Unsubscribe()
         {
