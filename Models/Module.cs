31cd3cbbcf859d704a64c70eace5806f193abe39
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-09-15 23:23:13 +0200

    Updated methods in Module.cs

--- Models/Module.cs
+++ Models/Module.cs
@@ -1,3 +1,4 @@
+using CampusLearn.Models;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -5,27 +6,16 @@ namespace CampusLearn.Models
 {
     public class Module
     {
-        internal object TutorExpertises;
-
         [Key]
         public int ModuleID { get; set; }
 
         [Required, StringLength(10)]
-        public required string ModuleCode { get; set; }
+        public string ModuleCode { get; set; }
 
         [Required, StringLength(100)]
-        public required string ModuleName { get; set; }
-        public object Topics { get; internal set; }
-        public object Expertises { get; internal set; }
+        public string ModuleName { get; set; }
 
-        public Topic AddTopic(string title)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Expertise AssignTutor(Tutor tutor, string expertiseLevel)
-        {
-            throw new System.NotImplementedException();
-        }
+        public Topic AddTopic(string title) { return null; }
+        public Expertise AssignTutor(Tutor tutor) { return null; }
     }
 }
