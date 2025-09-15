7c7f60e1e16bdd9cb3fbb3d119fafb718f9d2663
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-09-15 23:24:26 +0200

    Minor update in Expertise

--- Models/Expertise.cs
+++ Models/Expertise.cs
@@ -9,14 +9,12 @@ namespace CampusLearn.Models
         public int ExpertiseID { get; set; }
 
         [ForeignKey("Tutor")]
-        public virtual  Tutor Tutor { get; set; }
         public int TutorID { get; set; }
 
         [ForeignKey("Module")]
         public int ModuleID { get; set; }
 
-        public required string ExpertiseLevel { get; set; }
-        public object Module { get; internal set; }
+        public string ExpertiseLevel { get; set; }
 
         public void UpdateExpertiseLevel(string level)
         {
