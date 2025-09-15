816ad006417654f4b18696990ea6e8c4c245ead0
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-09-15 16:05:55 +0200

    Created the code skeleton for the Admin Model.

---
+++ Models/Admin.cs
@@ -0,0 +1,17 @@
+using Microsoft.Identity.Client;
+
+namespace CampusLearn.Models
+{
+    public class Admin : User
+    {
+        public void ManageUser(User user, string action)
+        {
+
+        }
+
+        public void ModerateContent(Post post)
+        {
+
+        }
+    }
+}
