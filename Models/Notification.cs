7179f81e6a9aa0a176b8686f402ee4bdcbbdc5b6
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-09-15 23:34:50 +0200

    Defined enum inside file

--- Models/Notification.cs
+++ Models/Notification.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CampusLearn.Models
 {
+    public enum NotificationType { EMAIL, SMS }
     public class Notification
     {
         [Key]
@@ -13,33 +14,20 @@ namespace CampusLearn.Models
         public int StudentID { get; set; }
 
         [ForeignKey("Tutor")]
-        public int? TutorID { get; set; }
+        public int TutorID { get; set; }
 
         [Required]
-        public required string Message { get; set; }
+        public string Message { get; set; }
 
         [Required]
-        public Enums.NotificationType Type { get; set; }
+        public NotificationType Type { get; set; }
 
         public DateTime DateSent { get; set; } = DateTime.Now;
 
-        public virtual required Student Student { get; set; }
-        public virtual required Tutor Tutor { get; set; }
 
         public void Send()
         {
-            switch (Type)
-            {
-                case Enums.NotificationType.EMAIL:
-                    // Send email logic
-                    break;
-                case Enums.NotificationType.SMS:
-                    // Send SMS logic
-                    break;
-                case Enums.NotificationType.WHATSAPP:
-                    // Send WhatsApp message logic
-                    break;
-            }
+            
         }
     }
 }
